1439e472f0dc0d9160e4ffa04b2421e791159358
matasukef <f-e@toki.waseda.jp>
2017-11-28 13:08:16 +0900

    fixt

--- src/generator/extract_subject.py
+++ src/generator/extract_subject.py
@@ -9,10 +9,11 @@ def extract_subject(sentence):
         
         surface = token.surface
         part1, part2, part3, part4 = token.part_of_speech.split(',')
-        #print(surface, part1, part2, part3, part4)
+        print(surface, part1, part2, part3, part4)
         if len(norm) and part2 == '格助詞':
             return norm
 
+        #if part1 == '名詞' and part2 != '非自立':
         if part1 == '名詞' and part2 != '非自立':
             norm += surface
         else:
