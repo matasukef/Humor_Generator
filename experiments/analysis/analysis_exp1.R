b7b1668c77e90ecb800a843eec1cdc1d2a968e99
kosuke futamata <f-e@toki.waseda.jp>
2017-12-18 02:58:47 +0900

    update it

--- experiments/analysis/analysis_exp1.R
+++ experiments/analysis/analysis_exp1.R
@@ -1,5 +1,3 @@
-test_data <- read.csv('../../../../media/matasuke/Ubuntu01/Projects/Humor_Generator/experiments/experiment1/static/data/test2.csv')
-
 exp1 <- read.csv('~/../../../../media/matasuke/Ubuntu01/Projects/Humor_Generator/experiments/experiment1/static/result/exp1_result.csv')
 
 pre_survey <- read.csv('~/../../../../media/matasuke/Ubuntu01/Projects/Humor_Generator/experiments/experiment1/static/result/pre_survey_result.csv')
@@ -14,7 +12,7 @@ data <- merge(pre_survey_scores, exp1, by.x=c('age', 'name', 'sex'))
 #merge result date with test_data
 #merged_data = merge(data, test_data, by.x=c('image'))
 
-EXP_NUM = 5
+EXP_NUM = 50
 SIMS = c('origin', 'll', 'lh', 'hl', 'hh')
 
 # calculate sum of all subjects for each sims
@@ -31,12 +29,68 @@ for(i in SIMS){
   #transform(humor_scores, label=score)
 }
 
-# calculate each sims frequency
-sims_frequenct = data.frame()
-for(i in SIMS){
-  label = paste('cap_', i, sep="")
-  for(j in 1:EXP_NUM){
-    name = paste('exp1_q', j, '_', i, sep="")
-    print(data[name])
-  }
+cap_scores_sum = humor_scores[6:10]
+cap_scores_mean = cap_scores_sum / EXP_NUM
+
+#mean scores by col
+each_cap_mean = apply(cap_scores_mean, MARGIN=2, mean)
+
+
+# calculate each sims frequency to validate it is based on normal disctibution.
+#sims_frequenct = data.frame()
+#for(i in SIMS){
+#  label = paste('cap_', i, sep="")
+#  for(j in 1:EXP_NUM){
+#    name = paste('exp1_q', j, '_', i, sep="")
+#    print(data[name])
+#  }
+#}
+
+
+# origin caps
+origin_caps = data.frame(data[1:3])
+for(i in 1:EXP_NUM){
+  name = paste('exp1_q', i, '_origin', sep="")
+  origin_caps = data.frame(origin_caps, data[name])
 }
+
+# low low caps
+ll_caps = data.frame(data[1:3])
+for(i in 1:EXP_NUM){
+  name = paste('exp1_q', i, '_ll', sep="")
+  ll_caps = data.frame(ll_caps, data[name])
+}
+
+# low high caps
+lh_caps = data.frame(data[1:3])
+for(i in 1:EXP_NUM){
+  name = paste('exp1_q', i, '_lh', sep="")
+  lh_caps = data.frame(lh_caps, data[name])
+}
+
+# high low caps
+hl_caps = data.frame(data[1:3])
+for(i in 1:EXP_NUM){
+  name = paste('exp1_q', i, '_hl', sep="")
+  hl_caps = data.frame(hl_caps, data[name])
+}
+
+# high high caps
+hh_caps = data.frame(data[1:3])
+for(i in 1:EXP_NUM){
+  name = paste('exp1_q', i, '_hh', sep="")
+  hh_caps = data.frame(hh_caps, data[name])
+}
+
+
+
+#calc frequency for origin caps
+origin_caps_frequency = data.frame(origin_caps[1:3])
+freq_table = 0
+for(i in origin_caps[4:53]){
+    freq= table(i)
+    freq_table = rbind(freq_table, freq)
+}
+
+rownames(freq_table) <- matrix(1:51)
+
